04309751c8150cb382f42a20ab0d29ddd24f6629
wugangL <wu15020302017@163.com>
2019-11-09 10:53:55 +0800

    on wlbranch commit by wugangL

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main(String[] args){
+	System.out.println("Hello World");
+	System.out.println("Hello wlbranch");
+	System.out.println("Hello wugangL");
+	System.out.println("Hello branch");
+	}
+}
